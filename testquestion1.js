67e90ca427f85145f75deb0d393e7bc8c1284cdf
javen2015 <zqlin15@gmail.com>
2025-10-27 10:15:16 +0000

    27/10

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,21 @@
+let posX, xSpeed,direction;
 
+function setup(){
+    createCanvas(600, 400);
+    background('cyan')
+    posX = width/ 2;
+    direction = "right"
+    xSpeed = 5;
+    fill('limegreen');
+}
+function draw(){
+    createCanvas(600,400);
+    background('cyan')
+    fill('limegreen');
+    circle(posX,height/2,50)
+    posX += xSpeed
+    if (posX - 50/2 <= 0 || posX + 50/2 >= width) {
+        xSpeed *= -1;
+    }
+
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,15 @@
+let pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
+let yPos;
 
+function setup(){
+    createCanvas(200, 300);
+    fill("pink");
+    textAlign(CENTER)
+    textSize(20)
+    yPos = 30;
+}
+function draw(){
+    for (let i = 0; i < pokemons.length; i++) {
+        text(pokemons[i-1],width/2,yPos * i)
+    }
+}
